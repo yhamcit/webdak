3f320825c8abed0c655adfff4f73d43a9b67e996
yhamcit <itadmin@cdyhamc.com>
2023-11-30 18:30:06 +0800

    adding vue webui

---
+++ webui/vite.config.ts
@@ -0,0 +1,55 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      // 需要去解析的文件
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // imports 指定自动引入的包位置（名）
+      imports: ['vue', 'pinia', 'vue-router'],
+      // 生成相应的自动导入json文件。
+      eslintrc: {
+        // 启用
+        enabled: true,
+        // 生成自动导入json文件位置
+        filepath: './.eslintrc-auto-import.json',
+        // 全局属性值
+        globalsPropValue: true
+      },
+      resolvers: [ArcoResolver()]
+    }),
+    Components({
+      // imports 指定组件所在目录，默认为 src/components
+      dirs: ['src/components/', 'src/view/'],
+      // 需要去解析的文件
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        // ArcoResolver({
+        //   sideEffect: true
+        // })
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
